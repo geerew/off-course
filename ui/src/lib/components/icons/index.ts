69616026ccf9fa8e236fcedbfefa0b5dfa00bf07
Michael Bell <michaelrbell@ymail.com>
2025-01-15 11:40:42 +0000

    ui: Add additional icons

--- ui/src/lib/components/icons/index.ts
+++ ui/src/lib/components/icons/index.ts
@@ -1,4 +1,7 @@
+export { default as Course } from './course.svelte';
 export { default as Edit } from './edit.svelte';
 export { default as Logo } from './logo.svelte';
 export { default as RightArrow } from './right-arrow.svelte';
 export { default as RightChevron } from './right-chevron.svelte';
+export { default as Tag } from './tag.svelte';
+export { default as User } from './user.svelte';
