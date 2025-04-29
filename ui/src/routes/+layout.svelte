404085ee3f95f4513e3bbea0c7fa0c16768e9598
Michael Bell <michaelrbell@ymail.com>
2025-04-29 11:08:16 +0100

    ui: rework root layout to differ for auth vs everything else

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -4,18 +4,52 @@
 	import '../app.css';
 
 	import { dev } from '$app/environment';
+	import { page } from '$app/state';
+	import { auth } from '$lib/auth.svelte';
 	import { TailwindIndicator } from '$lib/components';
+	import { Header, Oops, Spinner } from '$lib/components/';
 	import { Tooltip } from 'bits-ui';
 	import { Toaster } from 'svelte-sonner';
 
 	let { children } = $props();
+
+	const isAuthPath = page.url.pathname.startsWith('/auth');
+
+	$effect(() => {
+		if (isAuthPath) return;
+
+		auth.me();
+
+		const interval = setInterval(() => {
+			auth.me();
+		}, 3000);
+
+		return () => clearInterval(interval);
+	});
 </script>
 
 <Toaster theme="dark" richColors />
 
 <main>
 	<Tooltip.Provider>
-		{@render children()}
+		{#if !isAuthPath}
+			{#if auth.error !== null}
+				<div class="container-px flex w-full">
+					<Oops message={'Failed to fetch user: ' + auth.error} />
+				</div>
+			{:else if auth.user === null}
+				<div class="flex w-full justify-center pt-14 sm:pt-20">
+					<Spinner class="bg-foreground-alt-3 size-6" />
+				</div>
+			{:else}
+				<Header />
+				<div class="min-h-dvh pt-[calc(var(--height-header)+1px)]">
+					{@render children()}
+				</div>
+			{/if}
+		{:else}
+			{@render children()}
+		{/if}
 	</Tooltip.Provider>
 
 	{#if dev}
