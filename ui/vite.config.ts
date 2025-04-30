f486bfefdc4a2b24d5a0c04642db92b19cbf73b9
Michael Bell <michaelrbell@ymail.com>
2025-04-30 13:23:52 +0100

    ui: update add vidstack and remove postcss.config.js

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { vite as vidstack } from 'vidstack/plugins';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [vidstack(), tailwindcss(), sveltekit()],
 	server: {
 		hmr: {
 			// In dev mode, this forces the socket to use a port other than the default client
