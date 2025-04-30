ce307e1833a17131fadd8ae00e901751ee573632
Michael Bell <michaelrbell@ymail.com>
2025-04-30 13:21:26 +0100

    ui: add more icons

---
+++ ui/src/lib/components/icons/dot.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { SVGAttributes } from 'svelte/elements';
+	let restProps: SVGAttributes<SVGElement> = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	{...restProps}
+>
+	<circle cx="12.1" cy="12.1" r="1" />
+</svg>
