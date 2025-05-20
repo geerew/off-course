f74db46ff27724456834e93278be4dd03d51a928
Michael Bell <michaelrbell@ymail.com>
2025-05-20 15:13:06 +0100

    ui: dropdown radio group/item

--- ui/src/lib/components/sort-menu.svelte
+++ ui/src/lib/components/sort-menu.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-	import {
-		SortAscendingIcon,
-		SortDescendingIcon,
-		TickIcon,
-		WarningIcon
-	} from '$lib/components/icons';
+	import { SortAscendingIcon, SortDescendingIcon, WarningIcon } from '$lib/components/icons';
 	import RightChevron from '$lib/components/icons/right-chevron.svelte';
 	import { Dropdown } from '$lib/components/ui';
 	import type { SortColumns, SortDirection } from '$lib/types/sort';
-	import { DropdownMenu } from 'bits-ui';
 
 	type Props = {
 		columns: SortColumns;
@@ -41,7 +35,7 @@
 	});
 </script>
 
-<Dropdown.Root>
+<Dropdown.Root open={true}>
 	<Dropdown.Trigger class="w-36 [&[data-state=open]>svg]:rotate-90">
 		{#if columns.length === 0}
 			<div class="flex items-center gap-1.5">
@@ -73,74 +67,47 @@
 			</div>
 		{:else}
 			<div class="flex flex-col gap-1.5 p-1">
-				<DropdownMenu.RadioGroup bind:value={selectedColumn}>
+				<Dropdown.RadioGroup bind:value={selectedColumn}>
 					{#each columns as column}
-						<DropdownMenu.RadioItem
-							class="text-foreground-alt-1 hover:text-foreground hover:bg-background-alt-2 inline-flex w-full cursor-pointer items-center gap-2.5 rounded-md px-1 py-1 text-sm duration-200 select-none"
+						<Dropdown.RadioItem
 							value={column.column}
 							onclick={() => {
 								if (selectedColumn === column.column) return;
 								onUpdate?.();
 							}}
 						>
-							{#snippet children({ checked })}
-								{#if checked}
-									<TickIcon class="text-foreground-alt-3 size-3.5 stroke-2" />
-								{:else}
-									<span class="size-3.5"></span>
-								{/if}
-
-								{column.label}
-							{/snippet}
-						</DropdownMenu.RadioItem>
+							{column.label}
+						</Dropdown.RadioItem>
 					{/each}
-				</DropdownMenu.RadioGroup>
+				</Dropdown.RadioGroup>
 			</div>
-			<DropdownMenu.Separator class="bg-background-alt-5 h-px w-full" />
+
+			<Dropdown.Separator />
+
 			<div class="flex flex-col gap-1.5 p-1">
-				<DropdownMenu.RadioGroup bind:value={selectedDirection}>
-					<DropdownMenu.RadioItem
-						class="text-foreground-alt-1 hover:text-foreground hover:bg-background-alt-2 inline-flex w-full cursor-pointer items-center gap-2.5 rounded-md px-1 py-1 text-sm duration-200 select-none"
+				<Dropdown.RadioGroup bind:value={selectedDirection}>
+					<Dropdown.RadioItem
 						value="asc"
 						onclick={() => {
 							if (selectedDirection === 'asc') return;
 							onUpdate?.();
 						}}
 					>
-						{#snippet children({ checked })}
-							{#if checked}
-								<TickIcon class="text-foreground-alt-3 size-3.5 stroke-2" />
-							{:else}
-								<span class="size-3.5"></span>
-							{/if}
+						<SortAscendingIcon class="text-foreground-alt-3 size-4" />
+						{columns.find((column) => column.column === selectedColumn)?.asc || 'Ascending'}
+					</Dropdown.RadioItem>
 
-							<SortAscendingIcon class="text-foreground-alt-3 size-4" />
-
-							{columns.find((column) => column.column === selectedColumn)?.asc || 'Ascending'}
-						{/snippet}
-					</DropdownMenu.RadioItem>
-
-					<DropdownMenu.RadioItem
-						class="text-foreground-alt-1 hover:text-foreground hover:bg-background-alt-2 inline-flex w-full cursor-pointer items-center gap-2.5 rounded-md px-1 py-1 text-sm duration-200 select-none"
+					<Dropdown.RadioItem
 						value="desc"
 						onclick={() => {
 							if (selectedDirection === 'desc') return;
 							onUpdate?.();
 						}}
 					>
-						{#snippet children({ checked })}
-							{#if checked}
-								<TickIcon class="text-foreground-alt-3 size-3.5 stroke-2" />
-							{:else}
-								<span class="size-3.5"></span>
-							{/if}
-
-							<SortDescendingIcon class="text-foreground-alt-3 size-4" />
-
-							{columns.find((column) => column.column === selectedColumn)?.desc || 'Descending'}
-						{/snippet}
-					</DropdownMenu.RadioItem>
-				</DropdownMenu.RadioGroup>
+						<SortDescendingIcon class="text-foreground-alt-3 size-4" />
+						{columns.find((column) => column.column === selectedColumn)?.desc || 'Descending'}
+					</Dropdown.RadioItem>
+				</Dropdown.RadioGroup>
 			</div>
 		{/if}
 	</Dropdown.Content>
