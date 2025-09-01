0edf17cc5b0d6345eeee2e99349a583fe178f360
Michael Bell <michaelrbell@ymail.com>
2025-09-01 16:25:14 +0100

    ui: fix attachment under menu

--- ui/src/lib/components/ui/attachments.svelte
+++ ui/src/lib/components/ui/attachments.svelte
@@ -29,6 +29,7 @@
 	<Dropdown.Content
 		class="text-foreground-alt-3 z-10 flex max-h-[10rem] w-auto max-w-xs overflow-y-scroll px-1.5 py-1"
 		align="start"
+		portalProps={{ disabled: true }}
 	>
 		{#each attachments as attachment, index}
 			{@const lastAttachment = attachments.length - 1 == index}
